ab11110ad8a06520114e0f4079d000809c66947e
alonso14ss <reiyan92@gmail.com>
2020-03-17 15:03:41 +0900

    바인딩 구성

--- app/src/main/java/co/kr/tjoeun/helloworld/a20200317_02_java_first/MainActivity.java
+++ app/src/main/java/co/kr/tjoeun/helloworld/a20200317_02_java_first/MainActivity.java
@@ -1,14 +1,40 @@
 package co.kr.tjoeun.helloworld.a20200317_02_java_first;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import co.kr.tjoeun.helloworld.a20200317_02_java_first.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+//        edittext에 타이핑 => 입력버튼 => 텍스트뷰에 출력
+//        +입력버튼 누르면 기존 입력내용 삭제
+
+        binding.inputEdt;
+        binding.outputTxt;
+        binding.pushBtn;
+
+        pushBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String inputLog = inputEdt.getText().toString();
+                outputTxt.setText(inputLog);
+                inputEdt.setText("");
+            }
+        });
+
     }
 }
