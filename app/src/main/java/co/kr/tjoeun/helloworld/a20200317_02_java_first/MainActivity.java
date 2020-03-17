00de14a8c2277fa7ac747d7b53e738662a76fe4c
alonso14ss <reiyan92@gmail.com>
2020-03-17 15:46:08 +0900

    비밀번호에 따른 토스트 띄우기

--- app/src/main/java/co/kr/tjoeun/helloworld/a20200317_02_java_first/MainActivity.java
+++ app/src/main/java/co/kr/tjoeun/helloworld/a20200317_02_java_first/MainActivity.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import co.kr.tjoeun.helloworld.a20200317_02_java_first.databinding.ActivityMainBinding;
 
@@ -30,8 +31,33 @@ public class MainActivity extends AppCompatActivity {
 
                 Log.d("버튼클릭","입력 버튼 눌림");
                 String inputLog = binding.inputEdt.getText().toString();
+
+                Log.d("입력값확인",inputLog);
                 binding.outputTxt.setText(inputLog);
                 binding.inputEdt.setText("");
+            }
+        });
+
+//        입력한 비밀번호가 1234 일때 로그인 버튼을 누르면 
+//        토스트로 => 로그인 성공
+//        그외의 비밀번호로 로그인 버튼을 누르면
+//        토스트로 잘못된 비밀번호 입니다
+        
+        binding.loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+                String inputPw = binding.pwEdt.getText().toString();
+
+
+//                string을 ==으로 비교하면 false처리가 됨
+                if (inputPw.equals("1234")){
+                    Toast.makeText(MainActivity.this, "로그인 성공", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Toast.makeText(MainActivity.this, "잘못된 비밀번호 입니다", Toast.LENGTH_SHORT).show();
+                }
+                
             }
         });
 
